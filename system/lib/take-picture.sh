83e7e7a77ca909d47d6db4dfcb8c86266e1272be
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-12-06 21:00:30 -0600

    Update take-picture.sh

--- system/lib/take-picture.sh
+++ system/lib/take-picture.sh
@@ -2,7 +2,11 @@
 
 # Take a picture using grim and slurp
 /usr/bin/paplay $SCREENSHOT_SOUND &
-
+if [ -d "Pictures/Screenshots" ] then
+    echo "exists"
+else
+    mkdir -p ~/Pictures/Screenshots
+fi
 filename=~/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png
 grim -g "$(slurp)" - | tee "$filename" | wl-copy
 if [ $NOTIFY_SCREENSHOT -eq 1 ]; then
