48a0bfa0a80d15d0df53e9f68ce5e1b07afc50bf
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-12-10 16:41:36 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 sudo cp -r ./etc/xdg /etc/xdg
 sudo cp -r ./system/lib /opt
 sudo cp -r ./udev/* etc/udev/rules.d
+
+cd /opt/system
+mkdir env
+cd ./env
+sudo python3 -m venv STT
+sudo ./STT/bin/pip3 install vosk
